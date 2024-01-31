d81fed520939738c09d77b3ce8fcecf01dc567fb
Mustapha Talbi <mustapha.talbi2002@gmail.com>
2024-01-31 21:45:53 +0100

    renter socket adjustments and notifications finally working for both renter and landlord

--- src/landlord/landlordSocketLogic.ts
+++ src/landlord/landlordSocketLogic.ts
@@ -1,34 +1,22 @@
 import axios from "axios";
-import { landlordNameSpace } from "server";
 
 let connectedLandlords: Record<string, any> = {}; // Use a dictionary for O(1) lookup
 
 const landlordNameSpaceLogic = (landlordNameSpace: any) => {
   landlordNameSpace.on("connection", (socket: any) => {
     socket.on("newLandlordConnected", (data: any) => {
-      console.log(data);
       if (!connectedLandlords[data.landlordSocketId]) {
         connectedLandlords[data.landlordSocketId] = {
           socketId: socket.id,
           landlordMail: data.landlordMail,
         };
-        landlordNameSpace.emit(
-          "connectedRentersCountUpdate",
-          Object.keys(connectedLandlords).length
-        );
-      } else {
-        landlordNameSpace.emit(
-          "connectedRentersCountUpdate",
-          Object.keys(connectedLandlords).length
-        );
       }
     });
+    //
     socket.on("refLanNotis", (data: any) => {
-      console.log("AAAAAAAAAAAAAAAAAAAAAAA");
       const landlord = connectedLandlords[data.data.landlordSocketId];
-      console.log(landlord);
       if (landlord && landlord.socketId) {
-        landlordNameSpace.to(landlord.socketId).emit("refreshData", "hello");
+        socket.to(landlord.socketId).emit("refreshData", "hello");
       }
     });
     //
--- src/renter/renterSocketLogic.ts
+++ src/renter/renterSocketLogic.ts
@@ -4,31 +4,28 @@ let connectedRenters: Record<string, any> = {}; // Use a dictionary for O(1) loo
 
 const renterNameSpaceLogic = (renterNameSpace: any) => {
   renterNameSpace.on("connection", (socket: any) => {
+    // function to add new connected renters to an array and save their socket id
     socket.on("newRenterConnected", (data: any) => {
-      if (!connectedRenters[data.renterId]) {
-        connectedRenters[data.renterId] = {
+      console.log(data);
+      if (!connectedRenters[data.renterSocketId]) {
+        connectedRenters[data.renterSocketId] = {
           socketId: socket.id,
-          renterMail: data.renterMail,
         };
-        renterNameSpace.emit(
-          "connectedRentersCountUpdate",
-          Object.keys(connectedRenters).length
-        );
-      } else {
-        renterNameSpace.emit(
-          "connectedRentersCountUpdate",
-          Object.keys(connectedRenters).length
-        );
       }
     });
-
-    socket.on("testing", async () => {
-      console.log("hello");
+    // Function To Refresh notifications on the front side
+    socket.on("refreshRenterNotifications", (data: any) => {
+      console.log(data);
+      const renter = connectedRenters[data.renterSocketId];
+      console.log(renter + "renterrrr");
+      if (renter && renter.socketId) {
+        socket.to(renter.socketId).emit("refreshData", "hello");
+      }
     });
-
+    // function to remove users from the connected array once disconnecte
     socket.on("renterDisconnected", (data: any) => {
-      if (connectedRenters[data.renterId]) {
-        delete connectedRenters[data.renterId];
+      if (connectedRenters[data.landlordSocketId]) {
+        delete connectedRenters[data.landlordSocketId];
         renterNameSpace.emit(
           "connectedRentersCountUpdate",
           Object.keys(connectedRenters).length
